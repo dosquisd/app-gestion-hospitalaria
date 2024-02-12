2f53c36355e4862248bc0b0d804e71d60d716067
JuanD <dosquisd123@gmail.com>
2024-02-11 21:35:21 -0500

    Correción de unos errores para guardar y leer el archivo

--- hospital_files.py
+++ hospital_files.py
@@ -1,4 +1,5 @@
 from hospital import Hospital, MedicalHistory
+from date import Date
 import pandas as pd
 import numpy as np
 
@@ -17,11 +18,11 @@ class HospitalFiles:
 				m_h.patient.document, m_h.patient.full_name, m_h.patient.sex,
 				m_h.patient.birthday, m_h.patient.blood_press, m_h.patient.temperature,
 				m_h.patient.o2_saturation, m_h.patient.frequency, m_h.patient.evolution_notes,
-				m_h.patient.lab_results, m_h.patient.prescription_drugs, HospitalFiles.change_to_nan(m_h.patient.diagnostic_imgs),
-				m_h.patient.chronic_disease, m_h.admitted, HospitalFiles.change_to_nan(m_h.discharged), 
-				HospitalFiles.change_to_nan(m_h.bed), HospitalFiles.change_to_nan(m_h.nums_days_stay)
+				m_h.patient.lab_results, m_h.patient.prescription_drugs, m_h.patient.diagnostic_imgs,
+				m_h.patient.chronic_disease, m_h.admitted, m_h.discharged, m_h.bed, m_h.nums_days_stay
 			]
 
+		df_patients.fillna('?', inplace=True)
 		df_patients.to_csv(r'files\patients.csv', sep=';')
 
 	@staticmethod
@@ -33,27 +34,32 @@ class HospitalFiles:
 
 		for i in range(len(df_patients)):
 			hospital.medical_histories.append(MedicalHistory())
-			hospital.medical_histories[-1].patient.document = df_patients['Document'][i]
+			hospital.medical_histories[-1].patient.document = str(df_patients['Document'][i])
 			hospital.medical_histories[-1].patient.full_name = df_patients['Full Name'][i]
 			hospital.medical_histories[-1].patient.sex = df_patients['Sex'][i]
-			hospital.medical_histories[-1].patient.birthday = df_patients['Birth Day'][i]
+			hospital.medical_histories[-1].patient.birthday = Date()
+			hospital.medical_histories[-1].patient.birthday.input_date(df_patients['Birth Day'][i])
+
 			hospital.medical_histories[-1].patient.blood_press = df_patients['Blood Press'][i]
-			hospital.medical_histories[-1].patient.temperature = df_patients['Temperature'][i]
-			hospital.medical_histories[-1].patient.o2_saturation = df_patients['O2 Saturation'][i]
-			hospital.medical_histories[-1].patient.frequency = df_patients['Breathing Rate'][i]
+			hospital.medical_histories[-1].patient.temperature = float(df_patients['Temperature'][i])
+			hospital.medical_histories[-1].patient.o2_saturation = float(df_patients['O2 Saturation'][i])
+			hospital.medical_histories[-1].patient.frequency = float(df_patients['Breathing Rate'][i])
 			hospital.medical_histories[-1].patient.evolution_notes = df_patients['Evolution Notes'][i]
 			hospital.medical_histories[-1].patient.lab_results = df_patients['Lab Results'][i]
 			hospital.medical_histories[-1].patient.prescription_drugs = df_patients['Prescription Drugs'][i]
-			hospital.medical_histories[-1].patient.diagnostic_imgs = df_patients['Diagnostic Images'][i] if df_patients['Diagnostic Images'][i] is np.nan else None
-			hospital.medical_histories[-1].patient.chronic_disease = df_patients['Chronic Disease'][i]
-			hospital.medical_histories[-1].admitted = df_patients['Admitted'][i]
-			hospital.medical_histories[-1].discharged = df_patients['Discharged'][i] if df_patients['Discharged'][i] is np.nan else None
-			hospital.medical_histories[-1].bed = df_patients['Bed'][i] if df_patients['Bed'][i] is np.nan else None
-			hospital.medical_histories[-1].nums_days_stay = df_patients['Stay Days'][i] if df_patients['Stay Days'][i] is np.nan else None
+			hospital.medical_histories[-1].patient.diagnostic_imgs = df_patients['Diagnostic Images'][i] if df_patients['Diagnostic Images'][i] != '?' else None
+			hospital.medical_histories[-1].patient.chronic_disease = bool(df_patients['Chronic Disease'][i])
+			hospital.medical_histories[-1].admitted = HospitalFiles.to_bool(df_patients['Admitted'][i])
+			hospital.medical_histories[-1].discharged = HospitalFiles.to_bool(df_patients['Discharged'][i]) if df_patients['Discharged'][i] != '?' else None
+			hospital.medical_histories[-1].bed = HospitalFiles.to_bool(df_patients['Bed'][i]) if df_patients['Bed'][i] != '?' else None
+			hospital.medical_histories[-1].nums_days_stay = HospitalFiles.to_bool(df_patients['Stay Days'][i]) if df_patients['Stay Days'][i] != '?' else None
 
 			if hospital.medical_histories[-1].bed:
 				hospital.beds_used += 1
-			
+
+	
 	@staticmethod
-	def change_to_nan(value: object) -> object:
-		return value if value is not None else np.nan
+	def to_bool(sentence: str) -> bool:
+		if sentence == 'False':
+			return False
+		return True
